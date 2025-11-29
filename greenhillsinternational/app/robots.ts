a1fcc632ab358b9a13d242c7c94f85c17d19b00c
OG-GY <iamkhalil2005@gmail.com>
2025-11-29 19:32:37 +0500

    FIX: Robot.ts

--- greenhillsinternational/app/robots.ts
+++ greenhillsinternational/app/robots.ts
@@ -2,11 +2,13 @@ import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: ['/api/', '/.next/'],
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/api/', '/.next/'],
+      },
+    ],
     sitemap: 'https://greenhillsinternational.com/sitemap.xml',
   };
 }
